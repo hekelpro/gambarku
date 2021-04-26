25ae3c2f9273a2962c855a1975b5aca15e0b25f6
Rizky <62897038+hekelpro@users.noreply.github.com>
2021-04-26 22:36:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Upload gambarmu disini">
     <title>Upload Your Image</title>
-    <link rel="stylesheet" href="style.css">
+
     <link rel="shortcut icon" href="https://j.top4top.io/p_1942soeck0.jpg"/>
     <style type="text/css">
       * {
